ca6864298e0b02c15956d54cc3a60f69403c67c8
adnan.drina@gmail.com <adnan.drina@gmail.com>
2020-12-22 16:57:51 +0100

    housekeeping

--- install.sh
+++ install.sh
@@ -5,30 +5,29 @@
 echo "Creating Argo CD namespace"
 oc apply -f argocd-namespace.yaml
 echo "Argo CD namespace created!"
-
+echo " "
 echo "Creating Argo CD OperatorGroup"
 oc apply -f argocd-og.yaml
 echo "Argo CD OperatorGroup created!"
-
+echo " "
 echo "Creating Argo CD config"
 oc apply -f instance/argocd-groups.yaml
 oc apply -f instance/argocd-scc.yaml
 oc apply -f instance/cluster-role-binding.yaml
 oc apply -f instance/gpg-keys-cm.yaml
 echo "Argo CD config created!"
-
+echo " "
 echo "Creating Argo CD Subscription"
 oc apply -f argocd-sub.yaml
 echo "Argo CD Subscription created!"
-wait 20
-
+echo " "
 echo "Deploying Argo CD CR"
 HOSTNAME=$(oc config view --minify -o jsonpath='{.clusters[*].cluster.server}' | rev | cut -d':' -f2 | rev | cut -b 6-)
 sed -i -e "s|.cluster-dd79.dd79.example.opentlc.com|$HOSTNAME|g" instance/argocd.yaml
 oc apply -f instance/argocd.yaml
 echo "Argo CD CR created!"
-
-echo "Searching for available routes"
-oc get routes -n argocd
-echo "connect to the route named argocd using your browser \
-and login using your openshift credentials"
+#echo " "
+#echo "Searching for available routes"
+#oc get routes -n argocd
+#echo "connect to the route named argocd using your browser \
+#and login using your openshift credentials"
